9301800bf862bd1013268e71b287e1f0f7d97276
Tobi208 <tobi208@github.com>
2024-05-31 19:16:32 +0200

    gh pages base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   vite: {
-      css: {
-        preprocessorOptions: {
-          sass: {
-            additionalData : '@use "~/assets/sass/_colors.sass" as *\n@use "~/assets/sass/_mixins.sass" as *'
-          }
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData : '@use "~/assets/sass/_colors.sass" as *\n@use "~/assets/sass/_mixins.sass" as *'
         }
       }
     }
+  },
+  app: {
+    baseURL: '/dicedistro/'
+  }
 })
